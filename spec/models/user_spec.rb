c02e23d836d9581eb329839a3fd8257583341869
Sarah Allen <sarah.sra1@gmail.com>
2013-11-12 15:50:36 -0600

    Finish user edit, update, index, and destroy actions

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,7 +1,5 @@
 require 'spec_helper'
 
-
-
 describe User do
   before { @user = User.new(name: "User 1", email: "user@example.com", password: "foobar", password_confirmation: "foobar") }
   subject { @user }
@@ -12,7 +10,20 @@ describe User do
   it { should respond_to(:password) }
   it { should respond_to(:password_confirmation) }
   it { should respond_to(:remember_token) }
+  it { should respond_to(:authenticate) }
+  it { should respond_to(:admin) }
+
   it { should be_valid }
+  it { should_not be_admin }
+
+  describe "with admin attribute set to 'true'" do
+    before do
+      @user.save!
+      @user.toggle!(:admin)
+    end
+
+    it { should be_admin }
+  end
 
   describe "when name is not present" do
     before { @user.name = " " }
